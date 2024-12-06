c4bd802ad2cea64b0456ca45cc585f57379d7f8e
Antoine Salamitou <36542864+antoine-salamitou@users.noreply.github.com>
2024-12-06 17:24:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-extras_require={
-    'sphinx': ['sphinx>=4.0', 'sphinx-rtd-theme']
-}
+from setuptools import setup, find_packages
+
+setup(
+    name="diamonds-price-prediction",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "sphinx>=4.0",
+        "sphinx-rtd-theme"
+    ],
+    extras_require={
+        "sphinx": ["sphinx>=4.0", "sphinx-rtd-theme"]
+    },
+)
